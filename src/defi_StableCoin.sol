2022a737e52ef52feb9507cdd17df6774054ec38
shreya <shreya@shreyas-MacBook-Air-2.local>
2023-09-04 21:01:15 +0530

    first commit

---
+++ src/defi_StableCoin.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+//Collateral: Exogenous (ETH & BTC)
+// Minting: Algorithmic
+//Relative Stability: Pegged to USD
+//This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system
+
+contract defi_StableCoin{}
